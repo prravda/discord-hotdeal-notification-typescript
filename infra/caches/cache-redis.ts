6e5bcbccb39c8c2f4a0668d9f3b70d94a70acfb4
prravda <sonofhuman20@hufs.ac.kr>
2023-07-23 00:06:32 +0900

    config: - updated env list in `env-config.ts` - filled env prop in redis connections

--- infra/caches/cache-redis.ts
+++ infra/caches/cache-redis.ts
@@ -1,7 +1,9 @@
 import Redis from 'ioredis';
+import { ENV_LIST } from '../env-config';
 
 export const redisConnection = new Redis({
     host: 'cache-redis',
     port: 6379,
+    password: ENV_LIST.REDIS_PASSWORD,
     maxRetriesPerRequest: null,
 });
--- infra/job-queue/job-queue-connection.ts
+++ infra/job-queue/job-queue-connection.ts
@@ -1,7 +1,9 @@
 import IORedis from 'ioredis';
+import { ENV_LIST } from '../env-config';
 
 export const jobQueueConnection = new IORedis({
     host: 'job-queue-redis',
     port: 6380,
+    password: ENV_LIST.REDIS_PASSWORD,
     maxRetriesPerRequest: null,
 });
