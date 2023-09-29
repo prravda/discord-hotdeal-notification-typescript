9336391d4a84c7f8bd5a0294c53b50aa5e65a7b4
prravda <sonofhuman20@hufs.ac.kr>
2023-09-30 03:52:44 +0900

    logging - added logger

--- src/job/keyword-notification-manager.ts
+++ src/job/keyword-notification-manager.ts
@@ -9,51 +9,75 @@ import { HOT_DEAL_SOURCE } from '../../infra/enums';
 import { KeywordSearchRepositoryInstance } from '../keyword-search/repository-instance';
 import { KeywordServiceInstance } from '../keywords/service-instance';
 import { createHash } from '../../helpers';
+import { LokiLogger } from '../../infra/logger/loki-logger';
 
 export const keywordNotificationManager = async (
     hotDeal: FmKoreaGeneralHotDeal | FmKoreaPopularHotDeal | PpomppuHotDeal,
     hotDealSource: HOT_DEAL_SOURCE
 ) => {
-    const triggeredKeywordList =
-        await KeywordSearchRepositoryInstance.getRepository().searchInSentence(
-            hotDeal.title
-        );
+    try {
+        const triggeredKeywordList =
+            await KeywordSearchRepositoryInstance.getRepository().searchInSentence(
+                hotDeal.title
+            );
 
-    const keywordAndUserInfo =
-        await KeywordServiceInstance.getService().getKeywordAndSubscriberListByKeywordHashList(
-            triggeredKeywordList.map<string>((keyword) => createHash(keyword))
-        );
+        LokiLogger.getLogger().info({
+            labels: {
+                origin: 'keyword-detection',
+            },
+            message: {
+                title: hotDeal.title,
+                triggeredKeywordList,
+            },
+        });
 
-    const notifications: Notification<
-        FmKoreaGeneralHotDeal | FmKoreaPopularHotDeal | PpomppuHotDeal
-    >[] = [];
+        const keywordAndUserInfo =
+            await KeywordServiceInstance.getService().getKeywordAndSubscriberListByKeywordHashList(
+                triggeredKeywordList.map<string>((keyword) =>
+                    createHash(keyword)
+                )
+            );
 
-    for (const eachKeyword of keywordAndUserInfo) {
-        const { keyword, users } = eachKeyword;
-        for (const user of users) {
-            const { userId, cameFrom } = user;
-            notifications.push({
-                destination: cameFrom,
-                userId,
-                hotDealSource,
-                triggeredKeyword: keyword,
-                relatedHotDeal: hotDeal,
-            });
+        const notifications: Notification<
+            FmKoreaGeneralHotDeal | FmKoreaPopularHotDeal | PpomppuHotDeal
+        >[] = [];
+
+        for (const eachKeyword of keywordAndUserInfo) {
+            const { keyword, users } = eachKeyword;
+            for (const user of users) {
+                const { userId, cameFrom } = user;
+                notifications.push({
+                    destination: cameFrom,
+                    userId,
+                    hotDealSource,
+                    triggeredKeyword: keyword,
+                    relatedHotDeal: hotDeal,
+                });
+            }
         }
-    }
 
-    if (notifications.length > 0) {
-        await discordNotificationJobQueue.addBulk(
-            notifications.map((notification) => {
-                return {
-                    name: 'DiscordNotification',
-                    data: notification,
-                    opts: {
-                        removeOnComplete: true,
-                        delay: 1_000,
-                    },
-                };
-            })
-        );
+        if (notifications.length > 0) {
+            await discordNotificationJobQueue.addBulk(
+                notifications.map((notification) => {
+                    return {
+                        name: 'DiscordNotification',
+                        data: notification,
+                        opts: {
+                            removeOnComplete: true,
+                            delay: 1_000,
+                        },
+                    };
+                })
+            );
+        }
+    } catch (e) {
+        const errorAsErrorObject = e as Error;
+        LokiLogger.getLogger().error({
+            label: {
+                origin: 'keyword-detection',
+            },
+            stack: `${errorAsErrorObject.stack}`,
+        });
+        throw e;
     }
 };
